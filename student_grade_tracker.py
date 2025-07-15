43550448f340d61c804191dd9b2fa40e0ed783e2
Albert Wakuteka <albert.wakuteka@hgs.hiddengeniusproject.org>
2025-07-15 10:05:58 -0400

    first commit

---
+++ student_grade_tracker.py
@@ -0,0 +1,7 @@
+class Student:
+    def __init__(self, name ,student_id, grades):
+        self.name = name
+        self.student_id = student_id
+        self.grades = grades
+    def __str__(self):
+        return f""
