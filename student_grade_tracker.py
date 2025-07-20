73bfcfb1768bdd59ebae1fe3840d38d76ee11550
AsomethingW <albert.wakuteka@hgs.hiddengeniusproject.org>
2025-07-20 14:21:30 -0400

    Update student_grade_tracker.py

--- student_grade_tracker.py
+++ student_grade_tracker.py
@@ -2,16 +2,21 @@ class Student:
     def __init__(self, name ,student_id, grades):
         self.name = name
         self.student_id = student_id
-        self.grades = grades
+        self.grades = 0
     def __str__(self):
-        return f"Student: {self.name}, ID: {self.student_id}, Avergae Grade: {self.grades}"
+        if self.grades == None:
+            print("No grades yet")
+        else:
+            return f"Student: {self.name}, ID: {self.student_id}, Avergae Grade: {self.grades}"
     
     def add_grade(self, grade):
-        finally
-    
-    def get_average(self):
-        if self.grades == 
-    
+        add_number = int(input("Enter a number 1-100. "))
+        if add_number >= 1 and add_number <= 100:
+            for x in range(0):
+                self.grades.append(f"{add_number}")
+        else:
+            print("N/A")        
+
     def has_passed(self):
         if self.grades >= 60:
             return True
@@ -29,6 +34,16 @@ class Student:
             return "D"
         else:
             return "F"
+
+student1 = Student(f"Kyle", "4851", list)
+print(student1)
+
+student1.add_grade(10)
+
+student1.has_passed()
+
+student1.get_letter_grade()
+
     
     def reset_grades():
 
